96befb310f34070fe365976204fca11a06b31360
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-05 09:34:35 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,26 +1,25 @@
 import math
 def q1():
-  num = input("Input an integer: ")
-  num = int(num)
-  num = num + 3
-  print(num)
+  inp = input("Input an integer: ")
+  num = int(inp)
+  end = num + 3
+  print(end)
 def q2():
   num = input("Input a number: ")
-  num = (num + "4")
+  num = num + '4'
   num = float(num)
   num = num + 2
   print(num)
 def q3():
-  num = input("Input a radius: ")
-  num = float(num)
-  num = num ** 2
-  num = num * 3.14
-  print(num)
+  rad = input("Input a radius: ")
+  rad1 = float(rad)
+  pi = 3.14
+  num = pi * (rad1 * rad1)
 def q4():
   num = input("Input a number: ")
   num = float(num)
   num = num * 12
-  num = math.floor(num)
+  num = int(num)
   print(num)
 def q5():
   num = input("Input an integer: ")
@@ -30,8 +29,8 @@ def q5():
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
